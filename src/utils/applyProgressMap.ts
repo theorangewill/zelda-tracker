65b075f4c481cede979282b8632fd9b13c3b855a
William Felipe <theorangewill@gmail.com>
2026-01-09 22:08:57 -0300

    fix: Fixed saving dungeons info bugs

--- src/utils/applyProgressMap.ts
+++ src/utils/applyProgressMap.ts
@@ -12,6 +12,22 @@ export default function applyProgressMap(base: GameDataSchema, progressMap: Reco
       if (v && typeof v === 'object') {
         if ('completed' in v && k in progressMap) {
           v.completed = Boolean(progressMap[k]);
+          if ('compass' in v && `${k}_compass` in progressMap) {
+            v.compass = Boolean(progressMap[`${k}_compass`]);
+          }
+          if ('map' in v && `${k}_map` in progressMap) {
+            v.map = Boolean(progressMap[`${k}_map`]);
+          }
+          if ('boss_key' in v && `${k}_boss_key` in progressMap) {
+            v.boss_key = Boolean(progressMap[`${k}_boss_key`]);
+          }
+          if ('item' in v && `${k}_item` in progressMap) {
+            v.item.completed = Boolean(progressMap[`${k}_item`]);
+          }
+          if ('boss' in v && `${k}_boss` in progressMap) {
+            v.boss.completed = Boolean(progressMap[`${k}_boss`]);
+            continue;
+          }
         }
         walk(v);
       }
--- src/utils/createProgressMap.ts
+++ src/utils/createProgressMap.ts
@@ -9,7 +9,25 @@ export default function createProgressMap(gameData: GameDataSchema): Record<stri
     for (const k of Object.keys(obj)) {
       const v = obj[k];
       if (v && typeof v === 'object') {
-        if ('completed' in v) tasks[k] = Boolean(v.completed);
+        if ('completed' in v) {
+          tasks[k] = Boolean(v.completed);
+          if ('compass' in v && typeof v.compass === 'boolean') {
+            tasks[`${k}_compass`] = Boolean(v.compass);
+          }
+          if ('map' in v && typeof v.map === 'boolean') {
+            tasks[`${k}_map`] = Boolean(v.map);
+          }
+          if ('boss_key' in v && typeof v.boss_key === 'boolean') {
+            tasks[`${k}_boss_key`] = Boolean(v.boss_key);
+          }
+          if ('item' in v && v.item && typeof v.item === 'object' && 'completed' in v.item) {
+            tasks[`${k}_item`] = Boolean(v.item.completed);
+          }
+          if ('boss' in v && v.boss && typeof v.boss === 'object' && 'completed' in v.boss) {
+            tasks[`${k}_boss`] = Boolean(v.boss.completed);
+            continue;
+          }
+        }
         walk(v);
       }
     }
