217b3051c6623235a5163fee7697bad15f867ecf
William Felipe <theorangewill@gmail.com>
2026-01-09 22:08:40 -0300

    fix: Changed BrowserRouter to hashRouter for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import './styles.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/zelda-tracker">
+    <HashRouter>
       <div className="min-h-screen bg-gray-900">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/game/:gameId" element={<Game />} />
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
